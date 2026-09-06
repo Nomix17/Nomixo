2b9df719987a46c173a99957c622243ab97c0cfa
Nomix17 <izayrit12345678@gmail.com>
2026-09-06 16:34:08 +0100

    feat(loadingPage): add splash screen to login page and refactor logic to shared utils

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -197,6 +197,7 @@ function bindEvents() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  handleSplashScreen();
   handleFullScreenIcon();
   initEyeButtons();
   bindEvents();
--- src/shared/sharedFuncs.js
+++ src/shared/sharedFuncs.js
@@ -1386,6 +1386,43 @@ function setLeftButtonStyle(buttonId) {
   buttonIcon.classList.add("active");
 }
 
+function isWhiteMode(rgb) {
+  const [r, g, b] = rgb.split(',').map(val => parseInt(val.trim(), 10));
+  const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
+  return luminance > 150;
+}
+
+async function handleSplashScreen() {
+  const splashDiv = document.getElementById("div-splash");
+  if (!splashDiv) return;
+
+  const isFirstLaunch = localStorage.getItem("isFirstLaunch") === null;
+  if (!isFirstLaunch) {
+    splashDiv.remove();
+    keyboardPressesHandling();
+    return;
+  }
+
+  const themeColors = await window.electronAPI.loadTheme();
+  const colorObj = themeColors.theme.find(item => "primary-color" in item);
+  const useDarkIcon = colorObj ? isWhiteMode(colorObj["primary-color"]) : false;
+
+  const logImg = document.getElementById("img-splash-logo");
+  logImg.src = "../../../assets/logo/" + (useDarkIcon ? "dark-icon.png" : "icon.png");
+
+  localStorage.setItem("isFirstLaunch", "false");
+  splashDiv.classList.add("show-splash");
+
+  const MIN_SPLASH_DURATION = 1000;
+  setTimeout(() => {
+    splashDiv.classList.remove("show-splash");
+    splashDiv.classList.add("hide-splash");
+    splashDiv.addEventListener("transitionend", () => {
+      splashDiv.remove();
+      keyboardPressesHandling();
+    } , { once: true });
+  }, MIN_SPLASH_DURATION);
+}
 
 // ################################### ICONS & ASSET LOADING ###################################
 async function fetchSvg(url) {
