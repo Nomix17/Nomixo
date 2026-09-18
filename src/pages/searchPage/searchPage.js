ddecc5b5092b0c1c89ed649349b0b9ffd610583e
Nomix17 <izayrit12345678@gmail.com>
2026-09-18 03:00:23 +0100

    feat(searchPage): add support for searching by IMDb ID

--- src/pages/searchPage/searchPage.js
+++ src/pages/searchPage/searchPage.js
@@ -20,49 +20,66 @@ async function loadData(){
   loadSearchInformation(apiKey);
 }
 
-function loadSearchInformation(apiKey){
-  fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${searchQuery}`)
-    .then(res => res.json())
-    .then(data => {
+function loadSearchInformation(apiKey) {
+  const searchByImdbIdQuery = `https://api.themoviedb.org/3/find/${searchKeyword}?api_key=${apiKey}&external_source=imdb_id`;
+  const searchByQuery = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${searchQuery}`;
+
+  Promise.allSettled([
+    fetch(searchByImdbIdQuery),
+    fetch(searchByQuery)
+  ])
+    .then(async ([imdbRes, queryRes]) => {
+      if (imdbRes.status === "rejected" || queryRes.status === "rejected")
+        throw new Error("We’re having trouble loading data.</br>Please Check your connection and refresh!");
+
+      if (imdbRes.value.status === 401 || queryRes.value.status === 401)
+        throw new Error("We’re having trouble loading data.</br>Please make sure your Authentication Key is valide!");
+
+      const imdbData = await imdbRes.value.json();
+      const queryData = await queryRes.value.json();
+
+      const mergedArray = [
+        ...(imdbData.movie_results || []),
+        ...(imdbData.tv_results || []),
+        ...(imdbData.person_results || []),
+        ...(imdbData.tv_episode_results || []),
+        ...(imdbData.tv_season_results || []),
+        ...(queryData.results || [])
+      ];
 
       MoviesRecommandationDiv.innerHTML = "";
       SeriesRecommandationDiv.innerHTML = "";
       FiguresRecommandationDiv.innerHTML = "";
       OtherRecommandationDiv.innerHTML = "";
-      if(Number(data.status_code) === 7) throw new Error("We’re having trouble loading data.</br>Please make sure your Authentication Key is valide!");
-      if(Number(data.total_results) === 0) throw new Error(`Cannot Found Any Media Named: ${searchKeyword}`)
+
+      if (mergedArray.length === 0) throw new Error(`Cannot Found Any Media Named: ${searchKeyword}`);
 
       RightmiddleDiv.classList.add("activate");
-      insertResultsElement(data);
-      if(MoviesRecommandationDiv.innerHTML.trim() === "") document.getElementById("MoviesRecommandationsContainer").remove();
-      if(SeriesRecommandationDiv.innerHTML.trim() === "") document.getElementById("SeriesRecommandationsContainer").remove();
-      if(FiguresRecommandationDiv.innerHTML.trim() === "") document.getElementById("FiguresRecommandationsContainer").remove();
-      if(OtherRecommandationDiv.innerHTML.trim() === "") document.getElementById("OtherRecommandationsContainer").remove();
+      insertResultsElement(mergedArray);
+      if (MoviesRecommandationDiv.innerHTML.trim() === "") document.getElementById("MoviesRecommandationsContainer").remove();
+      if (SeriesRecommandationDiv.innerHTML.trim() === "") document.getElementById("SeriesRecommandationsContainer").remove();
+      if (FiguresRecommandationDiv.innerHTML.trim() === "") document.getElementById("FiguresRecommandationsContainer").remove();
+      if (OtherRecommandationDiv.innerHTML.trim() === "") document.getElementById("OtherRecommandationsContainer").remove();
       globalLoadingGif.remove();
-      checkIfDivShouldHaveMoveToRightOrLeftButton([MoviesRecommandationDiv,SeriesRecommandationDiv,FiguresRecommandationDiv,OtherRecommandationDiv]);
+      checkIfDivShouldHaveMoveToRightOrLeftButton([MoviesRecommandationDiv, SeriesRecommandationDiv, FiguresRecommandationDiv, OtherRecommandationDiv]);
+    })
+    .catch(err => {
+      let RightmiddleDiv = document.getElementById("div-middle-right");
+      RightmiddleDiv.innerHTML = "";
+      let displayRefreshButton = err.message.includes("Check your connection");
 
-  }).catch(err=>{ 
-    let RightmiddleDiv = document.getElementById("div-middle-right");
-    RightmiddleDiv.innerHTML ="";
-    let displayRefreshButton = err.message === "Failed to fetch";
+      let WarningElement = DisplayWarningOrErrorForUser(err.message, displayRefreshButton);
+      RightmiddleDiv.appendChild(WarningElement);
+      globalLoadingGif.remove();
+      RightmiddleDiv.classList.add("activate");
 
-    if(err.message === "Failed to fetch"){
-      err.message = "We’re having trouble loading data.</br>Please Check your connection and refresh!";
-    }
-
-    let WarningElement = DisplayWarningOrErrorForUser(err.message,displayRefreshButton);
-    RightmiddleDiv.appendChild(WarningElement);
-    globalLoadingGif.remove();
-    RightmiddleDiv.classList.add("activate");
-
-    console.error(err)
-  });
+      console.error(err);
+    });
 }
 
-function insertResultsElement(data){
-  let searchResults = data.results;
-  let ContainerDivs = [MoviesRecommandationDiv,SeriesRecommandationDiv,FiguresRecommandationDiv,OtherRecommandationDiv];
-  insertMediaElements(searchResults,ContainerDivs,null,LibraryInformation)
+function insertResultsElement(data) {
+  const ContainerDivs = [MoviesRecommandationDiv,SeriesRecommandationDiv,FiguresRecommandationDiv,OtherRecommandationDiv];
+  insertMediaElements(data, ContainerDivs, null, LibraryInformation)
 }
 
 triggerLoadingGif();
